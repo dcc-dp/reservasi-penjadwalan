0222ab476ad2a1c5b02222cb372382321d525892
rahmanhia <rahmanhiarmdhni@gmail.com>
2026-05-23 02:27:35 +0800

    update: dashboard admin

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -2,12 +2,34 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Jadwal;
+use App\Models\Kursus;
+use App\Models\Materi;
+use App\Models\Paket;
+use App\Models\Pembayaran;
 use Illuminate\Http\Request;
 
 class HomeController extends Controller
 {
-    public function home()
+    // public function home()
+    // {
+    //     return redirect('admin.dashboard');
+    // }
+    public function dashboard()
     {
-        return redirect('admin.dashboard');
+        $paket = Paket::count();
+        $materi = Materi::count();
+        $kursus = Kursus::count();
+        $jadwal = Jadwal::count();
+
+        $pembayaran = Pembayaran::sum('total');
+
+        return view('dashboard', compact(
+            'paket',
+            'materi',
+            'kursus',
+            'jadwal',
+            'pembayaran'
+        ));
     }
 }
